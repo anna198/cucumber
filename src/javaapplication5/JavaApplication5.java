89ce210eeffe41a02f1a478069b903c6abd31657
1 <1@172.20.10.3>
2016-11-22 09:17:16 +0200

    4456

---
+++ src/javaapplication5/JavaApplication5.java
@@ -0,0 +1,75 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication5;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ *
+ * @author 1
+ */
+public class JavaApplication5 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        /* int a = 5;
+            int b = 4;
+            int c = 1;
+            int d = 9;
+            double e = 2.5;
+         */
+ /* String familyFio[] = {"Волошко Т.В.", "Волошко Р.Я.", "Еремина В.К."};
+        System.out.println(familyFio[0]); //моя мама
+        System.out.println(familyFio[1]); // мой папа
+        System.out.println(familyFio[2]); //моя бабушка */
+ /* Scanner in = new Scanner(System.in);
+        int[] nums = new int[4];
+        for (int i = 0; i < nums.length; i++) {
+            nums[i]=in.nextInt();
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.print(nums[i]);
+        }
+        System.out.println();*/
+ /* Scanner input = new Scanner(System.in); // Объявление Scaner
+        System.out.println("Ввести длину массива");
+    int size = input.nextInt(); // Читаем с клавиатуры размер массива и записываем в size
+    int array[] = new int[size]; // Создаем масиив int размером в size
+        System.out.println("Элемент массива: ");
+        // пройдемся по всему массиву, заполняя его
+        for (int i = 0; i< size; i++) {
+            array[i] = input.nextInt(); // Заполняем массив элементами, введенными с клавиатуры
+        }
+        System.out.println("Введенные элементы массива");
+    for (int i = 0; i< size; i++) {
+            System.out.print(" " + array[i]); // выводим на экран полученый массив
+    }
+            System.out.println();*/
+ /*        int i = 50;
+        String abc = Integer.toString(i);
+        System.out.println(abc);*/
+ /* String abc1 = "1998";
+try {
+    Integer i2 = Integer.valueOf(abc1);
+    System.out.println(i2);
+} catch (NumberFormatException e) {
+    System.out.println("Неверный формат строки!");
+} */
+ /*  double abc2 = 3.186;
+ int i = (int) abc2;
+        System.out.println(i); */
+ int i = 2016;
+ double abc3 = (double) (i);
+        System.out.println(abc3);
+    }
+}
+
+// TODO code application logic here
+
